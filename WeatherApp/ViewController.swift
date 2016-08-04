2f24300bb192a5aeefc0d12ce746e2f619e39582
wctjerry <wctjerry@wctjerrydemacbook-pro.local>
2016-08-04 21:23:40 +0800

    Original version

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,14 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var textView: UITextView!
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - API call to get data
+    func getWeatherData(city: String) {
+        // API Key and url
+        let API_KEY = "1ebebcdbe10ee40ae73b48e8de648272"
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=\(city)&units=metric&mode=json&APPID=\(API_KEY)"
+        
+        // prepare for request
+        let session = NSURLSession.sharedSession()
+        let weatherRequestURL = NSURL(string: url)
+        
+        // data request
+        let dataTask = session.dataTaskWithURL(weatherRequestURL!) { (data: NSData?, response: NSURLResponse?, error: NSError?) in
+            
+            do {
+                //print("Task started")
+                
+                let weatherData = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as! [String: AnyObject]
+                self.presentWeatherData(weatherData)
+            } catch {
+                print("JSON error: ", error)
+            }
+            
+        }
+        dataTask.resume()
+        
+    }
+    
+    
+    // MARK: - Click City Button Action
+    @IBAction func clickCityButton(sender: AnyObject) {
+        let city = sender.titleLabel!!.text!
+        getWeatherData(city)
+        
+    }
+    
+    // MARK: - Present weather data
+    func presentWeatherData(weatherData: [String: AnyObject]) {
+        dispatch_async(dispatch_get_main_queue()){
+//            print(weatherData["weather"]![0])
+            
+            let weather = weatherData["weather"]![0]
+            let condition = weather["main"]!! as! String
+            let temp = weatherData["main"]!["temp"]!! 
+            let textToShow = "Weather: " + condition + "\n" + "\(temp)"
+            self.textView.text = textToShow
+        }
+
     }
 
 
